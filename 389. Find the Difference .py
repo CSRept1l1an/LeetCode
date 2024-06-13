21abea4b20d77d540fe6ae829c7e8e56474e0cf5
Telman Yusifov <csyusifovtelman@gmail.com>
2024-06-13 22:32:44 +0400

    Task finished

--- 389. Find the Difference .py
+++ 389. Find the Difference .py
@@ -1,5 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        t = ''.join(sorted(t))
+        s = ''.join(sorted(s))
         for i in range(len(s)):
             if s[i] != t[i]:
                 return t[i]
