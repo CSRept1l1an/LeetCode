a1d40e83ee9ab5f84654e0af447162971d877dc8
Telman Yusifov <csyusifovtelman@gmail.com>
2024-06-20 18:05:04 +0400

    Task finished

--- 859. Buddy Strings .py
+++ 859. Buddy Strings .py
@@ -1,12 +1,22 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
 
-def buddyStrings(self, s: str, goal: str) -> bool:
-    changes = []
-    for i in range(len(s)):
-        if s[i] != goal[i]:
-            changes.append(s[i])
-            changes.append(goal[i])
+        if s == goal:
+            seen = set()
+            for char in s:
+                if char in seen:
+                    return True
+                seen.add(char)
+            return False
 
-    if changes == changes[::-1]:
-        return True
+        diffs = []
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                diffs.append((s[i], goal[i]))
 
+        if len(diffs) == 2 and diffs[0] == diffs[1][::-1]:
+            return True
 
+        return False
